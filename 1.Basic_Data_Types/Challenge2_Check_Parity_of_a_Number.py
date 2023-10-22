9dea61d9ff0b0edf65d11f0625d6de3cb4c8ae49
Hai Pham <ngochai285nd@gmail.com>
2023-10-22 10:21:44 +0700

    update Challenge2

--- 1.Basic_Data_Types/Challenge2_Check_Parity_of_a_Number.py
+++ 1.Basic_Data_Types/Challenge2_Check_Parity_of_a_Number.py
@@ -1,3 +1,6 @@
+# Problem Statement
+# Write a function that takes a number and returns a string that states whether the number is even or odd.
+
 def checkParity(num):
     if num % 2 == 0:
         return "even"
